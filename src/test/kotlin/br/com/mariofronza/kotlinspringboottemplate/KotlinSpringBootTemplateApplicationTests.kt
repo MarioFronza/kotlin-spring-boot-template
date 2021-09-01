d6013d3b958ea8040a8f99259c2c223592ea00de
mariofronza <mariofronza1@gmail.com>
2021-09-01 11:19:31 -0300

    first commit

---
+++ src/test/kotlin/br/com/mariofronza/kotlinspringboottemplate/KotlinSpringBootTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.mariofronza.kotlinspringboottemplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootTemplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
